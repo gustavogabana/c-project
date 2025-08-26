b507870fa3d132fa474f56e73c5632d91d2a8363
gustavogabana <gustavo_97@protonmail.com>
2025-08-26 16:54:16 -0300

    feat: user data input and output

---
+++ user_input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int number = 0;
+    printf("Digite um numero: ");
+    scanf("%d", &number); // lib stdio.h
+    printf("O numero digitado foi: %d\n", number);
+    return 0;
+}
