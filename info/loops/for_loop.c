d9f3b859005c82dd99000be9dcf24400f9eb420c
gustavogabana <gustavo_97@protonmail.com>
2025-08-27 19:10:04 -0300

    docs: progressive and regressive loops

--- info/loops/for_loop.c
+++ info/loops/for_loop.c
@@ -3,8 +3,13 @@
 int main() {
 
     for (int i = 0; i < 5; i++) {
-        printf("value: %d\n", i);
+        printf("++: %d\n", i);
     }
+    printf("Loop progressivo\n");
+    for (int i = 5; i >= 0; i--) {
+        printf("--: %d\n", i);
+    }
+    printf("Loop regressivo\n");
 
     return 0;
 }
