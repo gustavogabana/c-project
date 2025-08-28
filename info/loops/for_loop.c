a432ac559aa3f9a8f137a194c36a3352848cc0b0
gustavogabana <gustavo_97@protonmail.com>
2025-08-28 19:40:29 -0300

    docs: loop inside loop

--- info/loops/for_loop.c
+++ info/loops/for_loop.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
 int main() {
-
+    printf("Loop progressivo\n");
     for (int i = 0; i < 5; i++) {
         printf("++: %d\n", i);
     }
-    printf("Loop progressivo\n");
+    printf("Loop regressivo\n");
     for (int i = 5; i >= 0; i--) {
         printf("--: %d\n", i);
     }
-    printf("Loop regressivo\n");
+    printf("Loop aninhado\n");
+    for (int i = 0; i < 6; i++) {
+        printf("i: %d\n", i);
+        for (int j = 0; j < 6; j++){
+            printf("j: %d\n", j);
+        }
+    }
 
     return 0;
 }
