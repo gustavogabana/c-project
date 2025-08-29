18f90e02742eb2de9530a0a82544d7c61b017dae
gustavogabana <gustavo_97@protonmail.com>
2025-08-28 21:29:53 -0300

    fix: refactor logic with accumulator syntax (+=)

--- lista_3/ex2.c
+++ lista_3/ex2.c
@@ -4,8 +4,8 @@ int main() {
     int n = 0, result = 0;
     printf("Type in a number: ");
     scanf("%d", &n);
-    for (int i = 0; i <= n; i++) {
-        result = result + i;
+    for (int i = 1; i <= n; i++) {
+        result += i; // += accumulator
     }
     printf("The sum of 1 until %d is %d", n, result);
     return 0;
