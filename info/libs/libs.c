ef78d561654ca0a086aeb0fa1cd390173513b114
gustavogabana <gustavo_97@protonmail.com>
2025-08-30 14:20:50 -0300

    docs: math lib

---
+++ info/libs/libs.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int base = 2, expoente = 3;
+    printf("2 elevado a 3: %.2f\n", pow(2, 3));
+    printf("Raiz quadrada de 25: %.2f\n", sqrt(25));
+    return 0;
+}
