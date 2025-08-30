90e9fc11e0c257487aa00a655d58217175d19b8e
gustavogabana <gustavo_97@protonmail.com>
2025-08-30 14:34:34 -0300

    docs: libs stdlib e time

--- info/libs/libs.c
+++ info/libs/libs.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
+    // math.h
     int base = 2, expoente = 3;
     printf("2 elevado a 3: %.2f\n", pow(2, 3));
     printf("Raiz quadrada de 25: %.2f\n", sqrt(25));
+
+    // stdlib.h, time.h
+    srand(time(NULL));
+    int numero = rand() % 100;
+    printf("Numero aleatorico (0 ate 100): %d\n", numero);
+
+    char texto[] = "123";
+    int valor = atoi(texto);
+    printf("Convertido: %d\n", valor);
     return 0;
 }
