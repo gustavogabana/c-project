4c528b77f4d1496453faa955a47d007efd736ce8
gustavogabana <gustavo_97@protonmail.com>
2025-09-16 20:11:37 -0300

    dsa: vetores de string

--- estrutura_dados/array_vetores/array.c
+++ estrutura_dados/array_vetores/array.c
@@ -3,8 +3,13 @@
 int main() {
     int notas[5] = {10, 32, 9, 2, 3};
     int notas_length = sizeof(notas)/sizeof(notas[0]);
+    char palavras[2][50] = {"primeira palavra", "segunda palavra"};
+    int palavras_length = sizeof(palavras)/sizeof(palavras[0]);
     for (int i = 0; i < notas_length; i++) {
         printf("Notas do vetor: %d\n", notas[i]);
+    }
+    for (int i = 0; i < palavras_length; i++) {
+        printf("Palavras do vetor: %s\n", palavras[i]);
     }
     return 0;
 }
