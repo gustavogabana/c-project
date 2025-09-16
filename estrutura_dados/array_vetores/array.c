901b6648022e0decd6000c692c70611fc00b7f44
gustavogabana <gustavo_97@protonmail.com>
2025-09-16 20:31:48 -0300

    dsa: somando elementos adicionados no vetor

--- estrutura_dados/array_vetores/array.c
+++ estrutura_dados/array_vetores/array.c
@@ -11,14 +11,22 @@ int main() {
     for (int i = 0; i < palavras_length; i++) {
         printf("Palavras do vetor: %s\n", palavras[i]);
     }
-    char nomes[3][20];
-    int nomes_length = sizeof(nomes)/sizeof(nomes[0]);
-    for (int i = 0; i < nomes_length; i++) {
-        printf("Digite um nome: ");
-        scanf("%s", &nomes[i]);
+    // char nomes[3][20];
+    // int nomes_length = sizeof(nomes)/sizeof(nomes[0]);
+    // for (int i = 0; i < nomes_length; i++) {
+    //     printf("Digite um nome: ");
+    //     scanf("%s", &nomes[i]);
+    // }
+    // for (int i = 0; i < nomes_length; i++) {
+    //     printf("Nomes digitados: %s\n", nomes[i]);
+    // }
+    int pontos[5], soma = 0;
+    int pontos_length = sizeof(pontos)/sizeof(pontos[0]);
+    for (int i = 0; i < pontos_length; i++) {
+        printf("Digite os pontos da posicao %d: ", i+1);
+        scanf("%d", &pontos[i]);
+        soma += pontos[i];
     }
-    for (int i = 0; i < nomes_length; i++) {
-        printf("Nomes digitados: %s\n", nomes[i]);
-    }
+    printf("O valor total da soma eh %d", soma);
     return 0;
 }
